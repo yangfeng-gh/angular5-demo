a646e7cd80b8bc7c86a7c394e5618db2d8eceea0
yvan <57882634@qq.com>
2018-03-19 18:54:02 +0800

    angular demo

---
+++ src/app/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+
+@Injectable() // 当该服务依赖其他服务时（比如在构造函数中注入其他服务），必须使用@Injectable注解
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+}
